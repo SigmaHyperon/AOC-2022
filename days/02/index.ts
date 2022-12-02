be89805c34dba74b70f7ac53ad278400d3fd6445
SigmaHyperon <nano_tech@web.de>
2022-12-02 14:24:12 +0100

    refactoring

--- days/02/index.ts
+++ days/02/index.ts
@@ -2,17 +2,11 @@ import { Input, ListInput } from "../../lib/input";
 import Solver from "../../lib/solver";
 import "../../lib/prototypes";
 
-enum Action {
-	Rock,
-	Paper,
-	Scissors
-}
+enum Action { Rock, Paper, Scissors };
 
-enum Outcome {
-	Win,
-	Loss,
-	Draw
-}
+const beatingOrder = [ Action.Rock, Action.Paper, Action.Scissors, Action.Rock, Action.Paper ];
+
+enum Outcome { Win, Loss, Draw };
 
 type OpponentAction = "A" | "B" | "C";
 type YourAction = "X" | "Y" | "Z";
@@ -25,12 +19,6 @@ interface Turn {
 interface Actions {
 	opponent: Action;
 	you: Action;
-}
-
-interface Result {
-	played: Action;
-	outcome: Outcome;
-	score: number;
 }
 
 const opponentMap = {
@@ -61,93 +49,57 @@ outcomeScore.set(Outcome.Win, 6);
 outcomeScore.set(Outcome.Draw, 3);
 outcomeScore.set(Outcome.Loss, 0);
 
-function validateOpponentAction(action: string): action is OpponentAction {
+function isOpponentAction(action: string): action is OpponentAction {
 	return ["A", "B", "C"].includes(action);
 }
 
-function validateYourAction(action: string): action is YourAction {
+function isYourAction(action: string): action is YourAction {
 	return ["X", "Y", "Z"].includes(action);
 }
 
 function parse(line: string): Turn {
-	const _ = line.split(" ");
-	const opponentAction = _[0];
-	const yourAction = _[1];
-	if(validateOpponentAction(opponentAction) && validateYourAction(yourAction)) {
-		return {
-			opponent: opponentAction,
-			you: yourAction
-		}
+	const [opponentAction, yourAction] = line.split(" ");
+	if(isOpponentAction(opponentAction) && isYourAction(yourAction)) {
+		return { opponent: opponentAction, you: yourAction }
 	} else {
 		throw "invalid Action: " + opponentAction + " " + yourAction;
 	}
 }
 
-
-// const lines = Input.import(`A Y
-// B X
-// C Z`).asLines().removeEmpty().parse(parse);
 const lines = Input.readFile().asLines().removeEmpty().parse(parse);
 
 function determineOutcome(opponentAction: Action, yourAction: Action): Outcome {
 	if(opponentAction === yourAction) {
 		return Outcome.Draw;
+	} else if(yourAction === generateDesiredOutcome(opponentAction, Outcome.Win)) {
+		return Outcome.Win;
 	} else {
-		if(yourAction === Action.Rock && opponentAction === Action.Scissors || yourAction === Action.Paper && opponentAction === Action.Rock || yourAction === Action.Scissors && opponentAction === Action.Paper) {
-			return Outcome.Win
-		} else {
-			return Outcome.Loss;
-		}
+		return Outcome.Loss;
 	}
 }
 
-function simpleActions(turn: Turn): Actions {
+function translateTurnToActionsPart1(turn: Turn): Actions {
 	return {
 		opponent: opponentMap[turn.opponent],
 		you: simpleMap[turn.you]
 	};
 }
 
-function evaluateTurn(actions: Actions): Result {
+function evaluateTurn(actions: Actions): number {
 	const outcome = determineOutcome(actions.opponent, actions.you);
-	const _actionScore = actionScore.get(actions.you);
-	const _outcomeScore = outcomeScore.get(outcome);
-	return {
-		played: actions.you,
-		outcome: outcome,
-		score: _actionScore + _outcomeScore
-	};
+	return actionScore.get(actions.you) + outcomeScore.get(outcome);
 }
 
 function part1() {
-	return lines.get().map(v => evaluateTurn(simpleActions(v)).score).sum();
+	return lines.get().map(translateTurnToActionsPart1).map(evaluateTurn).sum();
 }
 
 function generateDesiredOutcome(opponentAction: Action, desiredOutcome: Outcome): Action {
-	if(desiredOutcome === Outcome.Draw) {
-		return opponentAction;
-	} else {
-		if(desiredOutcome === Outcome.Win) {
-			if(opponentAction === Action.Rock) {
-				return Action.Paper;
-			} else if(opponentAction === Action.Paper) {
-				return Action.Scissors;
-			} else {
-				return Action.Rock;
-			}
-		} else {
-			if(opponentAction === Action.Rock) {
-				return Action.Scissors;
-			} else if(opponentAction === Action.Paper) {
-				return Action.Rock;
-			} else {
-				return Action.Paper;
-			}
-		}
-	}
+	const outcomeOrder = [ Outcome.Draw, Outcome.Win, Outcome.Loss ];
+	return beatingOrder[(beatingOrder.indexOf(opponentAction) + outcomeOrder.indexOf(desiredOutcome))];
 }
 
-function complexActions(turn: Turn): Actions {
+function translateTurnToActionsPart2(turn: Turn): Actions {
 	return {
 		opponent: opponentMap[turn.opponent],
 		you: generateDesiredOutcome(opponentMap[turn.opponent], complexMap[turn.you])
@@ -155,7 +107,7 @@ function complexActions(turn: Turn): Actions {
 }
 
 function part2() {
-	return lines.get().map(v => evaluateTurn(complexActions(v)).score).sum();
+	return lines.get().map(translateTurnToActionsPart2).map(evaluateTurn).sum();
 }
 
 Solver.create()
